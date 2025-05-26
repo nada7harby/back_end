e00a7992712093dfadc34df1de698cf58b19ddf0
Manar-202 <119630678+Manar-202@users.noreply.github.com>
2025-05-26 12:47:09 +0300

    all

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -11,7 +11,7 @@ const authMiddleware = (req, res, next) => {
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.userId = decoded.userId;
+    req.userId = decoded.id;
     next();
   } catch (error) {
     return res.status(401).json({ message: 'Invalid token' });
