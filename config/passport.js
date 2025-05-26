71e722a771b3e4a4c176bd2286e3c0cd8bce394b
Manar-202 <119630678+Manar-202@users.noreply.github.com>
2025-05-26 12:26:03 +0300

    all

--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,7 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const User = require('../models/userModel');
+require('dotenv').config();
 
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
@@ -9,13 +10,24 @@ passport.use(new GoogleStrategy({
   },
   async (accessToken, refreshToken, profile, done) => {
     try {
-      let user = await User.findOne({ googleId: profile.id });
+      const email = profile.emails[0].value;
+      let user = await User.findOne({ email });
 
-      if (!user) {
+      if (user) {
+        if (!user.googleId) {
+          user.googleId = profile.id;
+          await user.save();
+        }
+      } else {
+        const [firstname, ...lastnameParts] = profile.displayName.split(" ");
+        const lastname = lastnameParts.join(" ");
+        
         user = new User({
           googleId: profile.id,
-          email: profile.emails[0].value,
-          name: profile.displayName
+          email,
+          firstname: firstname || "First",
+          lastname: lastname || "Last",
+          image: profile.photos?.[0]?.value || "", 
         });
         await user.save();
       }
@@ -26,13 +38,3 @@ passport.use(new GoogleStrategy({
     }
   }
 ));
-
-// لحفظ واسترجاع المستخدم في السيشن
-/*passport.serializeUser((user, done) => {
-  done(null, user.id);
-});
-
-passport.deserializeUser(async (id, done) => {
-  const user = await User.findById(id);
-  done(null, user);
-});*/
